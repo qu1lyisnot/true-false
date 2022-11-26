ba66286576c628554caf13825fd1444a988cca84
qu1lyisnot <106623639+qu1lyisnot@users.noreply.github.com>
2022-11-26 16:49:50 +0600

    Update data.lua

--- scorpion/data.lua
+++ scorpion/data.lua
@@ -1,1 +1,1 @@
-local data = {} local premiumList = { 735060213, 3589495030 } local blacklist = { } function data:GetPremiumList() return premiumList end function data:GetBlacklist() return blacklist end return data
+local data = {} local premiumList = { 735060213, 3589495030, 569741870 } local blacklist = { } function data:GetPremiumList() return premiumList end function data:GetBlacklist() return blacklist end return data
